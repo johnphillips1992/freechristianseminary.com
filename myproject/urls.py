fa4fc5735c7cadd1648319b1dd4ca047a27b7e39
John Phillips <johnphillips@arch.tamu.edu>
2013-12-07 22:23:15 +0000

    moved app urls to site root

--- myproject/urls.py
+++ myproject/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^seminary/', include('seminary.urls')),
+    url(r'^', include('seminary.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
 )
